0a1af2107f2688de4b19016028e1e495898454f5
James H. Nguyen <james.nguyen@flyr.com>
2025-04-26 13:02:36 -0700

    fix: last working checkpoint

--- code_agent/tools/verbosity.py
+++ code_agent/tools/verbosity.py
@@ -6,3 +6,4 @@ allowing tools to import verbosity utilities consistently.
 """
 
 # Re-export the verbosity controller from the main module
+from code_agent.verbosity import get_controller, VerbosityController, VerbosityLevel
